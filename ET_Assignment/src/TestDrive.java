1c5fcc16ca6d71ab72b9460ca6f58de63dd3169f
Kristopher Brown <kbb8134@rit.edu>
2017-12-01 19:40:43 -0500

    Added Printing functionality to the main

--- ET_Assignment/src/TestDrive.java
+++ ET_Assignment/src/TestDrive.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+
+
+
 /*
  * TestDrive.java
  */
@@ -27,6 +30,7 @@ public class TestDrive {
 		ArrayList<TableEntry> entries30Plus = new ArrayList<TableEntry>();
 		ArrayList<TableEntry> entries29Less = new ArrayList<TableEntry>();
 		int i = 0;
+		String prevState = "ZZ"; //ZZ will trigger an if statement in the printing step
 		
 		/*
 		 * Usage statement
@@ -65,11 +69,38 @@ public class TestDrive {
 		
 		}
 		
+		/*
+		 * Sorts both groups by state alphabetically to allow for easier grouping by states
+		 */
 		Collections.sort(entries29Less);
 		Collections.sort(entries30Plus);
 		
+		/*
+		 * Printing the groups individually, seperating by state
+		 */
+		
+		System.out.println("30+ Years Old");
+		System.out.println("----------------------");
+		
+		for(i = 0; i<entries30Plus.size(); i++){
+			if(!prevState.equals(entries30Plus.get(i).getState())){
+				prevState = entries30Plus.get(i).getState();
+				System.out.println("State: " + prevState);
+
+			}
+			System.out.println("Docunment: " + entries30Plus.get(i).toString());
+		}
+		
+		System.out.println();
+		
+		System.out.println("< 30 Years Old");
+		System.out.println("----------------------");
 		for(i = 0; i<entries29Less.size(); i++){
-			System.out.println(entries29Less.get(i).toString());
+			if(!prevState.equals(entries29Less.get(i).getState())){
+				prevState = entries29Less.get(i).getState();
+				System.out.println("State: " + prevState);
+			}
+			System.out.println("Docunment: " + entries29Less.get(i).toString());
 		}
 	}
 	
